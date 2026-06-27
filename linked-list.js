2433812c031501ebb5b2815a696caee3f176295a
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2026-06-27 17:59:23 -0400

    Create toString() function to print nodeList Create append() and prepend() functions to add nodes Create size() to return size of nodeList Create headNode() and tailNode() to return first and last nodes Test all of these class methods

--- linked-list.js
+++ linked-list.js
@@ -10,45 +10,107 @@ export default class LinkedList{
         this.head = null;
     }
 
+    toString(){
+        let result = "";
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            result += "( " + currentNode.value + " ) -> ";
+            if(currentNode.next === null){
+                result += "null";
+            }
+            currentNode = currentNode.next;
+        }
+        return result
+    }
+
     append(value){
+        const newNode = new Node(value);
+        
+        if(this.head === null){
+            this.head = newNode;
+            return;
+        }
 
+        let currentNode = this.head;
+        while(currentNode.next !== null){
+            currentNode = currentNode.next;
+        }
+
+        currentNode.next = newNode;
     }
 
-    prepend(){
 
+    prepend(value){
+        const newNode = new Node(value);
+
+        if(this.head === null){
+            this.head = newNode;
+            return;
+        }
+
+        //target the current head node.
+        let headNode = this.head;
+        //declare newNode as this.head.
+        this.head = newNode;
+        //have newNode.next = currentNode
+        newNode.next = headNode;
     }
+
 
     size(){
+        let nodeCount = 0;
 
+        if(this.head === null){
+            return `The amount of Nodes in this list are ${nodeCount}`;
+        }
+
+        let currentNode = this.head;
+        while(currentNode !== null){
+            nodeCount++;
+            currentNode = currentNode.next;
+        }
+
+        return `The amount of Nodes in this list are ${nodeCount}`;
+    }
+
+    
+    headNode(){
+        let head = this.head;
+        if(head === null){
+            return undefined;
+        } else{
+            return head.value;
+        }
     }
     
-    head(){
-
+    tailNode(){
+        let currentNode = this.head;
+        while(currentNode.next !== null){
+            currentNode = currentNode.next;
+        }
+        return currentNode.value;
     }
 
-    tail(){
-
-    }
 
     at(index){
 
     }
 
+
     pop(){
 
     }
+
 
     contains(value){
 
     }
 
+
     findIndex(value){
 
     }
 
-    toString(){
-
-    }
 
 
     //extras
@@ -56,7 +118,9 @@ export default class LinkedList{
 
     }
 
+
     removeAt(index){
 
     }
+
 }
--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 import LinkedList from "./linked-list.js";
 
+//test ALL list properties from LinkedList class
 const list = new LinkedList();
 
-//test ALL list properties from LinkedList class
-list.append();
-list.prepend();
-list.size();
-list.head();
-list.tail();
-list.at();
-list.pop();
-list.contains();
-list.findIndex();
-list.toString();
-
-
-list.insertAt();
-list.removeAt();
-
 //run with Node to see results 
+list.append(10);
+list.append(20);
+list.prepend(5);
+list.prepend(4);
+console.log(list.size());
+console.log(list.headNode());
+
+console.log(list.tailNode());
+// list.at();
+// list.pop();
+// list.contains();
+// list.findIndex();
+// list.toString();
+
+
+// list.insertAt();
+// list.removeAt();
+
 console.log(list.toString());
